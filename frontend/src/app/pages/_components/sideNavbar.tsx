15faa5c8bd6c0a6a703b0c7c6e3e9d3f59467ee3
Daniel <Danielromeo99@gmail.com>
2024-03-11 12:05:22 +0000

    minor changes

--- frontend/src/app/pages/_components/sideNavbar.tsx
+++ frontend/src/app/pages/_components/sideNavbar.tsx
@@ -4,19 +4,28 @@ import { AiOutlineMenuUnfold, AiOutlineMenuFold } from 'react-icons/ai';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 const SideNavbar = ()=>{
-    return (
+   
         
-    <Sidebar>
-    <Menu>
-        <SubMenu label="Charts">
-        <MenuItem> Pie charts </MenuItem>
-        <MenuItem> Line charts </MenuItem>
-        </SubMenu>
-        <MenuItem> Documentation </MenuItem>
-        <MenuItem> Calendar </MenuItem>
-    </Menu>
-    </Sidebar>
-    )
+        const [toggled, setToggled] = React.useState(false);
+
+        return (
+          <div style={{ display: 'flex', height: '100%', minHeight: '400px' }}>
+            <Sidebar onBackdropClick={() => setToggled(false)} toggled={toggled} breakPoint="always">
+                <button className="sb-button" onClick={() => setToggled(!toggled)}>
+                  Toggle
+                </button>
+              <Menu>
+                <MenuItem> Documentation</MenuItem>
+                <MenuItem> Calendar</MenuItem>
+                <MenuItem> E-commerce</MenuItem>
+                <MenuItem> Examples</MenuItem>
+              </Menu>
+            </Sidebar>
+            
+               
+              
+          </div>
+        )
 }
 
 export default SideNavbar;
