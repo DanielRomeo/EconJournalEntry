7b1011aa2f53b359f6d9d13401a668a354d70a1b
danielromeo <danielromeo99@gmail.com>
2024-03-13 11:00:29 +0200

    added sections on home page without styling

--- frontend/src/app/landing/landingComponent.tsx
+++ frontend/src/app/landing/landingComponent.tsx
@@ -1,7 +1,6 @@
 import LandingNavbar from "./navbarComponent";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import Styles from '../_styles/LandingPage/LandingComponent.module.scss'
-// import Image from "react-bootstrap";
 import Image from "next/image";
 
 const LandingComponent = ()=>{
@@ -20,7 +19,6 @@ const LandingComponent = ()=>{
                 </Container>
                 
             </div>
-
 
             {/* About secttion */}
             <div className={`${Styles.aboutSection}`}>
@@ -47,8 +45,90 @@ const LandingComponent = ()=>{
             </div>
 
             {/* Why you should join section */}
+            <div className={`${Styles.whyJoinSection}`}>
+                <Container className={`${Styles.whyJoinSectionContainer}`}>
+                    <Row className={`${Styles.headerRow}`}>
+                        <h1>Why you should join chatter</h1>
+                    </Row>
+
+                    <Row className={`${Styles.descriptionRow}`}>
+                        <p>Our goal is to make writers and readers see our platform as their next heaven for blogging, ensuring ease in interactions, connecting with like-minded peers, have access to favorite content based on interests and able to communicate your great ideas with people</p>
+                    </Row>
+
+                    <Row className={`${Styles.cardsRow}`}>
+                        <Col lg='4' md='4' sm='12'>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <Card.Link href="#">Card Link</Card.Link>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col lg='4' md='4' sm='12'>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <Card.Link href="#">Card Link</Card.Link>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col lg='4' md='4' sm='12'>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <Card.Link href="#">Card Link</Card.Link>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div> {/* End of why join section*/}
 
             {/* Person section */}
+            <div className={`${Styles.personSection}`}>
+                <Container>
+                    <Row>
+                        <Col lg='4' md='4' sm='12'>
+                            <Image
+                                src="/personSectionImage.jpg"
+                                width={400}
+                                height={400}
+                                alt="Picture of the author"
+                            />
+                        </Col>
+
+                        <Col lg='8' md='8' sm='12'>
+                            <p>
+                                "Chatter has become an integral part of my online experience. As a user of this incredible blogging platform, I have discovered a vibrant community of individuals who are passionate about sharing their ideas and engaging in thoughtful discussions.”
+                            </p>
+
+                            <h6>Adebobola Muhydeen, <small>Software developer at Apple</small> </h6>
+
+                            <Button>Join chatter</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
 
             {/* write, read, connect section */}
 
